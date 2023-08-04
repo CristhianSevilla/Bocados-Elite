102b7d11324f91d3629bf8993103ccb643454a1c
Cristhian Sevilla <crissev17@gmail.com>
2023-08-04 17:50:45 -0600

    Funcion para manipular cantidad de un producto

--- src/components/ModalProducto.js
+++ src/components/ModalProducto.js
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import Image from "next/image"
 import useBocados from "@/hooks/useBocados"
 import { formatearDinero } from "@/helpers"
@@ -5,6 +6,7 @@ import { formatearDinero } from "@/helpers"
 const ModalProducto = () => {
 
     const { producto, handleChangeModal } = useBocados()
+    const [cantidad, setCantidad] = useState(1)
 
     return (
         <div className="md:flex gap-10">
@@ -30,6 +32,34 @@ const ModalProducto = () => {
                 <p className="mt-5 font-black text-5xl text-amber-500">
                     {formatearDinero(producto.precio)}
                 </p>
+                <div className="flex gap-5 mt-5 text-amber-100">
+                    <button
+                        type="button"
+                        onClick={() => {
+                            if(cantidad <= 1) return
+                            setCantidad(cantidad - 1)
+                        }}
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                    </button>
+
+                    <p className="text-3xl">{cantidad}</p>
+
+                    <button
+                        type="button"
+                        onClick={() => {
+                            if(cantidad >= 10) return
+                            setCantidad(cantidad + 1)
+                        }}
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="#none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+
+                    </button>
+                </div>
             </div>
         </div>
     )
