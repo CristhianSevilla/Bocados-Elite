049eeac4f29ed1bb022ac2e138bc8f9273821921
Cristhian Sevilla <crissev17@gmail.com>
2023-08-08 12:17:58 -0600

    Insertando registros en la BD

--- src/pages/api/ordenes.js
+++ src/pages/api/ordenes.js
@@ -1,7 +1,21 @@
+import { PrismaClient } from "@prisma/client"; 
+
 export default async function handler(req, res) {
+
+    const prisma = new PrismaClient()
 
     console.log(req.body);
     if (req.method === "POST") {
-        res.json({ metodo: "Post" })
+
+        //Los datos de data deben coincidir con el modelo del esquema de prisma
+        const orden = await prisma.orden.create({
+            data: {
+                nombre: req.body.nombre,
+                fecha: req.body.fecha,
+                total: req.body.total,
+                pedido: req.body.pedido,
+            },
+        })
+        res.json(orden)
     }
 }
