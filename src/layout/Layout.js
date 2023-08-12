d6e2587b7754531c79417b3fc961fe07bac832ff
Cristhian Sevilla <crissev17@gmail.com>
2023-08-12 13:27:09 -0600

    Estilos

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -38,12 +38,12 @@ export default function Layout({ children, pagina }) {
       </Head>
 
       <div className="md:flex">
-        <aside className="md:w-5/12 xl:w-1/4 2xl:w-1/5 bg-amber-300">
+        <aside className="md:w-5/12 xl:w-1/4 2xl:w-1/5 bg-amber-300 md:h-screen ">
           <Sidebar />
         </aside>
 
         <main className="md:w-7/12 xl:w-3/4 2xl:w-4/5  ">
-          <div className="py-10 px-5 md:px-8 xl:px-12 h-screen overflow-y-scroll w-full">
+          <div className="py-10 px-5 md:px-8 xl:px-12 md:h-screen md:overflow-y-scroll w-full">
             <Pasos />
             {children}  
         
