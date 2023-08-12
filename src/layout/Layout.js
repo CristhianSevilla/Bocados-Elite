560f2ea31a46e8147e0aa636aca230bce330f974
Cristhian Sevilla <crissev17@gmail.com>
2023-08-12 12:53:38 -0600

    Sidebar Responsive

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -38,11 +38,11 @@ export default function Layout({ children, pagina }) {
       </Head>
 
       <div className="md:flex ">
-        <aside className="md:w-5/12 xl:w-4/12 2xl:w-1/5 bg-amber-300">
+        <aside className="md:w-4/12 xl:w-1/4 2xl:w-1/5 bg-amber-300">
           <Sidebar />
         </aside>
 
-        <main className="md:w-7/12 xl:w-8/12 2xl:w-4/5 h-screen overflow-y-scroll">
+        <main className="md:w-8/12 xl:w-3/4 2xl:w-4/5 h-screen overflow-y-scroll">
           <div className="py-10 px-5 md:px-8 xl:px-12">
             <Pasos />
             {children}  
